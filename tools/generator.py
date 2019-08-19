b78482bc47ee3e74be1af138fe7a803317e151bb
datastory.cz <honza@datastory.cz>
2019-08-19 16:52:20 +0200

    korektury tabulka

--- tools/generator.py
+++ tools/generator.py
@@ -6,13 +6,8 @@ import json
 d = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vS7JPfqUa3UYJ5gbvp4Kp_-fJcdaOX3sUoTt4njFC5D0NKh68WFoG8PBptGdjPwO-vtmPYxZCSBFCPJ/pub?gid=0&single=true&output=csv')
 
 #%%
-d.head()
-
-#%%
 d.fillna('', inplace=True)
 
 #%%
 with open('./data/data.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(list(d.to_dict(orient='index').values()),  ensure_ascii=False))
-
-#%%
